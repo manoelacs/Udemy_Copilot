23762e4279bf2f6f49f280210c418310ff75ec00
manoela <manoelacs@ic.ufal.br>
2024-11-02 10:11:58 +0100

    fix(dbTest.ts): correct import statement for EmployeeRepository and clean up formatting

--- backend/test/dbTest.ts
+++ backend/test/dbTest.ts
@@ -1,22 +1,17 @@
-import { EmplRepository } from '../src/repository/employeeRespository';
+import { EmployeeRepository } from '../src/repository/employeeRespository';
 
+async function main() {
+  const emplRepository = new EmployeeRepository();
 
-
-async function main() { 
-    const emplRepository = new EmplRepository();
-    
- const addResult = await emplRepository.addEmployee({
-        firstName: 'John',
-        lastName: ' Smith',
-        hireDate: new Date(),
-        position: 'Employ',
-        
-    });
-    console.log(addResult   ? addResult : null  );
-    const employee = await emplRepository.getEmployees();
-    console.log(employee);
-
+  const addResult = await emplRepository.addEmployee({
+    firstName: 'John',
+    lastName: ' Smith',
+    hireDate: new Date(),
+    position: 'Employ',
+  });
+  console.log(addResult ? addResult : null);
+  const employee = await emplRepository.getEmployees();
+  console.log(employee);
 }
 
 main();
-    
